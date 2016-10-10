6476fc3b3cd694c5fc52fba9108b23d47049165e
BettinaP <bettinaprophete@gmail.com>
2016-10-09 21:53:25 -0400

    initial commit

---
+++ WeatherApp/DarkSkyAPIClient.swift
@@ -0,0 +1,13 @@
+//
+//  DarkSkyAPIClient.swift
+//  WeatherApp
+//
+//  Created by Bettina on 10/9/16.
+//  Copyright © 2016 Bettina Prophete. All rights reserved.
+//
+
+import Foundation
+
+class DarkSkyAPIClient {
+
+}
